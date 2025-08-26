e69c2b8d45db397c6b58faa7f9d77677165440a0
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2025-08-27 03:00:09 +0500

    setup Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
    return (
       <main className="p-4 ">
          <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me</Button>
+         <Button>Click me!</Button>
       </main>
    );
 }
